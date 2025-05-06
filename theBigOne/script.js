d48d526a75dec786ca0602a0ba24bb2d8d692376
joaquin-sanchez456 <joaquin.insano456@gmail.com>
2025-05-06 01:11:09 -0500

    Archivos principales

---
+++ theBigOne/script.js
@@ -0,0 +1,6 @@
+let botonInicioContinuar = document.querySelector("#continuarInicio");
+
+botonInicioContinuar.addEventListener("click", function () {
+    window.location.href = "pagina2.html";
+    return;
+});
---
+++ theBigOne/script2.js
@@ -0,0 +1,6 @@
+let botonDescarga = document.querySelector("#enlacesDescarga");
+
+botonDescarga.addEventListener("click", function () {
+    window.location.href = "descargas.html";
+    return;
+});
